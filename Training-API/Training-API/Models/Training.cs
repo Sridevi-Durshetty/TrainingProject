ca673e56debc0d0135031bf4dbd1310e7edcac74
NITYASREE-PC\sride <vdurshetty@gmail.com>
2019-07-15 23:42:58 +1000

    Training entity added

---
+++ Training-API/Training-API/Models/Training.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Training_API.Models
+{
+    [Table("tblTraining")]
+    public class Training 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string TrainingName { get; set; }
+
+        [Required]
+        public DateTime? StartDate { get; set; }
+
+        [Required]
+        public DateTime? EndDate { get; set; }
+      
+    }
+}
