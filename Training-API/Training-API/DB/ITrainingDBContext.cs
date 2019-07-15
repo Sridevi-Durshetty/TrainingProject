38e9e7cffaa42ec5d4c41d153ed4c936ca6b01b1
NITYASREE-PC\sride <vdurshetty@gmail.com>
2019-07-15 23:59:49 +1000

    Created folder structure for DB , Filters, Repositories

---
+++ Training-API/Training-API/DB/ITrainingDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Training_API.DB
+{
+    public class ITrainingDBContext
+    {
+    }
+}
---
+++ Training-API/Training-API/Repositories/TrainingRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Training_API.Repositories
+{
+    public class TrainingRepository
+    {
+    }
+}
