a70461e87c8033c5aa8abbf007d62fe1add170a9
NITYASREE-PC\sride <vdurshetty@gmail.com>
2019-07-16 00:13:24 +1000

    Added constant errror messages and implemented model validation

--- Training-API/Training-API/Models/Training.cs
+++ Training-API/Training-API/Models/Training.cs
@@ -4,24 +4,32 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using Training_API.Utils;
 
 namespace Training_API.Models
 {
     [Table("tblTraining")]
-    public class Training 
+    public class Training : IValidatableObject
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ErrorMessageConstants.TrainingNameRequired)]
         public string TrainingName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ErrorMessageConstants.StartDateRequired)]
         public DateTime? StartDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ErrorMessageConstants.EndDateRequired)]
         public DateTime? EndDate { get; set; }
-      
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (EndDate < StartDate)
+            {
+                yield return new ValidationResult(ErrorMessageConstants.DateCompareInvalid);
+            }
+        }
     }
 }
---
+++ Training-API/Training-API/Utils/ErrorMessageConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Training_API.Utils
+{
+    public class ErrorMessageConstants
+    {
+        public const string TrainingNameRequired = "Training Name is required.";
+        public const string StartDateRequired = "Start Date is required.";
+        public const string EndDateRequired = "End Date is required.";
+        public const string DateCompareInvalid = "End Date cannot be less than Start Date.";
+        public const string TrainingNotInserted = "Error while adding into DB";
+    }
+}
