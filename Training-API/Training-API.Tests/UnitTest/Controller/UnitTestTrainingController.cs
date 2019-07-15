cdfe4037c01d1f1f78fc2bc4b3dffb667571dcc6
NITYASREE-PC\sride <vdurshetty@gmail.com>
2019-07-16 00:37:21 +1000

    implemented unit test for valid iput

--- Training-API/Training-API.Tests/UnitTest/Controller/UnitTestTrainingController.cs
+++ Training-API/Training-API.Tests/UnitTest/Controller/UnitTestTrainingController.cs
@@ -2,16 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.Results;
 using Training_API.Controllers;
 using Training_API.DB;
+using Training_API.Models;
 using Training_API.Tests.UnitTest.Common;
 
 namespace Training_API.Tests.UnitTest.Controller
 {
     [TestClass]
-    class UnitTestTrainingController
+    public class UnitTestTrainingController
     {
         TrainingController objTrainingController;
         ITrainingDBContext testDBContext = new TestTrainingDBContext();
@@ -19,6 +22,23 @@ namespace Training_API.Tests.UnitTest.Controller
         private void CreateInstance()
         {
             objTrainingController = new TrainingController(testDBContext);
+        }
+
+        [TestMethod]
+        public void Validate_Training_Valid_Test()
+        {
+            CreateInstance();
+            Training t = new Training
+            {
+                TrainingName = "Dotnet",
+                StartDate = Convert.ToDateTime("2019/01/10"),
+                EndDate = Convert.ToDateTime("2019/01/20")
+            };
+            var result = objTrainingController.AddTraining(t) as NegotiatedContentResult<string>;
+
+            Assert.AreEqual(result.StatusCode, HttpStatusCode.OK);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Content, "10");
         }
     }
 }
