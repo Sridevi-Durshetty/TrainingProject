3363e6743ab899f06f37ce997bd9d5b6bfc01ee7
NITYASREE-PC\sride <vdurshetty@gmail.com>
2019-07-17 07:12:17 +1000

    validation method in component

--- Training-Web/src/app/training.component.ts
+++ Training-Web/src/app/training.component.ts
@@ -18,50 +18,80 @@ export class TrainingComponent implements OnInit {
     'TrainingName': {
       'required': 'Training Name is required',
       'minlength': 'information must be more than 3 chars',
-      'pattern':'Training Name should be characters'
+      'pattern': 'Training Name should be characters'
     },
     'StartDate': {
       'required': 'Start date is required',
-      'undefined':'Invalid date'
-      
+      'undefined': 'Invalid date'
+
     },
     'EndDate': {
-      'required': 'End date is required'      
+      'required': 'End date is required'
     },
     'DateGroup': {
       'dateCompare': 'End Date should be greater than Start Date'
-    }    
+    }
   };
 
   formErrors = {
     'TrainingName': '',
     'StartDate': '',
     'EndDate': '',
-    'DateGroup':''
+    'DateGroup': ''
   };
-  private newTraining : Training
+  private newTraining: Training
   constructor(private refTrainingService: TrainingService,
-                private trainingFB: FormBuilder) {
+    private trainingFB: FormBuilder) {
 
-    this.datePickerConfig = Object.assign({}, 
-      { 
-        containerClass: 'theme-dark-blue', 
+    this.datePickerConfig = Object.assign({},
+      {
+        containerClass: 'theme-dark-blue',
         showWeekNumbers: false,
-        minDate: new Date(2018,0,1),
+        minDate: new Date(2018, 0, 1),
         dateInputFormat: 'YYYY/MM/DD'
-      })    
+      })
 
-   }
+  }
 
   ngOnInit() {
 
     //created formgroup and formcontrols with validators using formbuilder
     this.trainingFG = this.trainingFB.group({
-      TrainingName: ['', [Validators.required, Validators.minLength(3),Validators.pattern('[a-zA-Z ]*')]],
+      TrainingName: ['', [Validators.required, 
+                          Validators.minLength(3), 
+                          Validators.pattern('[a-zA-Z ]*')]],
       DateGroup: this.trainingFB.group({
-          StartDate: ['', Validators.required],
-          EndDate: ['', [Validators.required]]
-          })
+            StartDate: ['', Validators.required],
+            EndDate: ['', [Validators.required]]
+      })
+    });
+
+    this.trainingFG.valueChanges.subscribe((data)=>{
+      this.logValidationErrors(this.trainingFG)
+    })    
+
+  }
+
+  logValidationErrors(group: FormGroup= this.trainingFG): void {   
+    Object.keys(group.controls).forEach((key: string) => {
+     
+      const abstractControl = group.get(key);
+        this.formErrors[key] = '';       
+        if (abstractControl && !abstractControl.valid &&
+          (abstractControl.touched || abstractControl.dirty)) {      
+          const messages = this.validationMessages[key];
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {  
+              console.log('Key = ' + key + ' && Value = ' + abstractControl.value);             
+              this.formErrors[key] += messages[errorKey] == null ? 'Invalid Data ' : messages[errorKey] + ' ';
+            }
+          }
+        }
+        console.log('form errror ',this.formErrors); 
+
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+      } 
     });
   }
 
